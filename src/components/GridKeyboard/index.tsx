c21fbeee2e9923fdd9bd8be54801b44dd779f91f
zlatan <zlatan.pham@gmail.com>
2018-11-27 22:20:16 +0700

    render keyboard layout

--- src/components/GridKeyboard/index.tsx
+++ src/components/GridKeyboard/index.tsx
@@ -1,30 +1,57 @@
-import * as React from "react";
-import styles from "./style.module.scss";
-import { data, Key } from "./keycodes";
-import styled from "react-emotion";
-
-const mapData = data.map((d, i) => {
-  return { ...d, columnStart: i * d.span + 1, columnEnd: (i + 1) * d.span + 1 };
-});
+import * as React from 'react';
+import styles from './style.module.scss';
+import { data, Key } from './keycodes';
+import styled from 'react-emotion';
 
 interface CodeProps {
   columnStart: number;
   columnEnd: number;
+  rowStart: number;
+  rowEnd: number;
 }
 
-const Code = styled("div")<CodeProps>(props => ({
+interface MappedKey extends CodeProps, Key {}
+
+const keys: MappedKey[] = data.reduce(
+  (a: MappedKey[], c: Key[], rowIndex: number) => {
+    let colSpan = 0;
+    let prevColSpan = 0;
+    const k = c.map((column: Key, colIndex: number) => {
+      if (!column.sameColumn) {
+        prevColSpan = colSpan;
+        colSpan += column.columnSpan;
+      }
+      return {
+        ...column,
+        columnStart: prevColSpan + 1,
+        columnEnd: colSpan + 1,
+        rowStart: rowIndex * 4 + column.rowStartAt + 1,
+        rowEnd: rowIndex * 4 + column.rowSpan + column.rowStartAt + 1,
+      };
+    });
+    return [...a, ...k];
+  },
+  [],
+);
+
+const Code = styled('div')<MappedKey>(props => ({
   gridColumnStart: `${props.columnStart} `,
   gridColumnEnd: `${props.columnEnd} `,
-  border: "1px solid #ddd"
+  gridRowStart: props.rowStart,
+  gridRowEnd: props.rowEnd,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  border: '1px solid #ddd',
 }));
 
 const GridKeyboard = () => {
   return (
     <div className={styles.container}>
       <div className={styles.grid}>
-        {mapData.map((key, index: number) => (
-          <Code key={key.code} {...key}>
-            {key.name}
+        {keys.map(props => (
+          <Code key={props.code} {...props}>
+            {props.name}
           </Code>
         ))}
       </div>
