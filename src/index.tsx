9c2287c5ef6f43a139cc73b2edb2cffb98d09e15
zlatan <zlatan.pham@gmail.com>
2018-12-01 18:43:28 +0700

    add module hot loader

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render = () => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+};
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// Render Once
+render();
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    render();
+  });
+}
